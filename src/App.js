ffc50e23321adf9b7c030644927ad0553b1580be
danigj86 <48217639+danigj86@users.noreply.github.com>
2022-02-13 20:45:44 +0100

    UserContext creado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Auth } from './components/Auth';
+import { useUserContext } from './context/userContext';
 
 function App() {
 
-
-  const [user, setUser] = useState(false);
+  const { user, error, loading } = useUserContext();
 
   return (
-   <div className="App"> {user ? <Dashboard/> : <Auth />}</div> 
+    <div className="App">
+      {error && <p className="error">{error}</p>}
+      {loading ? <h2>Loading...</h2> : <div className="App"> {user ? <Dashboard/> : <Auth />}</div>  }
+    </div>
   )
 }
 
